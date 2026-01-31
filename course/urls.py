8f09c6a98abe8395631233bd488d4d21364c9a2a
LeNguyenXeng <tungnguyenle0909@gmail.com>
2026-01-31 11:30:44 +0700

    feat: create API Create Course and API Detail Course

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from course.views import get_courses
+from course.views import get_courses, create_course, get_course_by_id
 
 urlpatterns = [
     path('', get_courses),
+    path('create/', create_course),
+    path('detail/<int:id>/', get_course_by_id),
 ]
--- course/views.py
+++ course/views.py
@@ -1,5 +1,6 @@
 from django.core.serializers import serialize
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -12,4 +13,26 @@ from course.serializers import CourseSerializer
 def get_courses(request):
     courses = Course.objects.all()
     serializer = CourseSerializer(courses, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_course(request):
+    serializer = CourseSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            serializer.data,
+            status = status.HTTP_201_CREATED
+        )
+
+    return Response(
+        serializer.errors,
+        status = status.HTTP_400_BAD_REQUEST
+    )
+
+@api_view(['GET'])
+def get_course_by_id(request, id):
+    course = Course.objects.get(id=id)
+    serializer = CourseSerializer(course)
     return Response(serializer.data)
